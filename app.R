30e25d651c91081caf1009e2d8d52ea846c24cac
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2022-11-27 15:55:12 +0100

    Create app.R

---
+++ app.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ 
+
+ui <- fluidPage(
+  
+  div(style="display:inline-block; height:10%; vertical-align:center;position:absolute; left: 1310px",
+                    sliderInput(inputId = "var4", label = "Variable D", min = 0, max = 100, step = 10, value = 20)),
+                
+                div(style="display:inline-block; height:10%; vertical-align:center;position:absolute;right:.2em", 
+                    sliderInput(inputId = "var5", label = "Variable E", min = 100, max = 500, step = 100, value = 50)),
+  
+  
+  
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({ 
+    x * 5
+  })
+}
+
+
+shinyApp(ui, server)
+
+
+
