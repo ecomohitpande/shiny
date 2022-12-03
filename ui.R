f32d7119b366b28362c15b0d4ebcd3b439a8b339
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2022-12-03 22:05:17 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,9 +1,13 @@
-ui <- fluidPage(
-  
-  div(style="display:inline-block; height:10%; vertical-align:center;position:absolute; left: 1310px",
-                    sliderInput(inputId = "var4", label = "Variable D", min = 0, max = 100, step = 10, value = 20)),
-                
-                div(style="display:inline-block; height:10%; vertical-align:center;position:absolute;right:.2em", 
-                    sliderInput(inputId = "var5", label = "Variable E", min = 100, max = 500, step = 100, value = 50)),
-    tableOutput("product")
-)
+ui=navbarPage("mongo query",
+              sidebarLayout(
+                sidebarPanel(
+                 selectInput("country1", "Select Cluster Country", unique(mong$aggregate('[{"$group": {"_id":"$country1"}}]'))),
+                 # selectInput("country", 'country', choices = "", selected = ""),
+                 
+                #  selectInput("country", 'country', choices = "", selected = ""),
+                ),
+                mainPanel(
+                  verbatimTextOutput(outputId = "text1"),
+                  dataTableOutput(outputId = "qry_results")
+                )
+              ))
