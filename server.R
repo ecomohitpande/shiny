9463e8b4c2d965f1cfa443db3b3c7913fcb71f53
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2022-12-03 22:06:04 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,40 @@
-server <- function(input, output, session) {
-  output$product <- renderTable({
-    d <- read.csv("d.csv")
+server=function(input, output, session) {
+  
+  # observeEvent(
+  #   input$cluster,
+  #   updateSelectInput(session, "country", "Select Country",
+  #                     choices = data$country[data$cluster==input$cluster]))
+  # 
+  # observeEvent(
+  #   mong$aggregate('[{"$group": {"_id":"$country1"}}]'),
+  #   updateSelectInput(session, "country", "Select Country",
+  #                     choices = mong$aggregate('[{"$group": {"_id":"$country"}}]')[mong$aggregate('[{"$group": {"_id":"$country1"}}]')==mong$aggregate('[{"$group": {"_id":"$country1"}}]')]))
+  # 
+  qryResults <- reactive({
     
-    d    
+    observeEvent(
+      mong$aggregate('[{"$group": {"_id":"$country1"}}]'),
+      updateSelectInput(session, "country", "Select Country",
+                        choices = mong$aggregate('[{"$group": {"_id":"$country"}}]')[mong$aggregate('[{"$group": {"_id":"$country1"}}]')==mong$aggregate('[{"$group": {"_id":"$country1"}}]')]))
     
+      
+    region <- list(country = input$country1)
+
+    qry <- paste0('{ "country1" : "',region,'"}')
+    df <- loadData(qry)
+    return(df)
+    df= data.frame(df)
+
   })
+  
+  output$qry_results <- renderDataTable({
+   qryResults()
+     
+      # p %>% filter(source== "Eurasian Bullfinch")
+    
+     
+  })
+  
+  output$text1 <- renderText(nrow(qryResults()))
+  
 }
