789cdf3783262d3a5474f59ce6956e3442a73187
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2022-12-04 19:42:59 +0100

    Delete ui.R

--- ui.R
+++
@@ -1,9 +0,0 @@
-ui=fluidPage(
-  selectInput(inputId = "boro", label = "Borough",choices = data),
-  selectInput(inputId ="cuis", label ="country", choices = "", selected = ""),
-  
-  mainPanel(
-    tableOutput(outputId = "table")
-  )
-  
-)
